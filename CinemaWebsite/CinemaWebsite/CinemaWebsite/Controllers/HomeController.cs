130e44d99c20c73209be35ac3cb922132c3945c8
TranDanh <danh.tran.huu.vr.mmlab.uit@gmail.com>
2015-05-12 22:18:58 +0700

    first commit project

---
+++ CinemaWebsite/CinemaWebsite/CinemaWebsite/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CinemaWebsite.Models;
+
+namespace CinemaWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        CinemaEntities db = new CinemaEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeaderInfoPartial()
+        {
+            var listHostesFilm = db.FILM.Take(1).ToList();
+            return PartialView(listHostesFilm);
+        }
+	}
+}
